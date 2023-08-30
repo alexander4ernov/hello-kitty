eed0e134bf90526c6b842d2fd36459352b812ec8
alexander4ernov <143617168+alexander4ernov@users.noreply.github.com>
2023-08-30 16:13:05 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Люблю тебя❤️😘😘😘';
+});
